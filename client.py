c18de18de58523f93d9562d3413dce9952a1aeb0
brrcrites <brrcrites@gmail.com>
2019-07-16 00:20:06 -0700

    Basic active process reporting using globals

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+import requests
 
 def get_current_top():
     # For Mac/FreeBSD only probably, one sample and no columns to only get global info
@@ -11,6 +12,7 @@ def get_current_top():
     num_processes_string = pattern.search(top_output)
     # Take the result (we should check we have one) and then split it at the space
     num_processes, total_string = num_processes_string.group().split(' ')
-    print(num_processes)
+    r = requests.post(url = 'http://127.0.0.1:5000/processes/send', data = { 'num_proc': num_processes } )
+    return num_processes
 
 get_current_top()
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request, session
 import requests
 import json
 
 from gcp_status import GCPStatus
 
 app = Flask(__name__)
+app.secret_key = 'terrible secret key' # Needed for using sessions
+
+num_proc = 0
 
 GCP_STATUS_URL = 'https://status.cloud.google.com/incidents.json'
 AWS_STATUS_URL = ''
@@ -34,6 +37,18 @@ def gcp_incidents_page():
     for index,obj in enumerate(status_objects):
         page += str(obj) + '<br>'
     return '<h1>GCP Incidents</h1>' + page
+
+@app.route('/processes/send', methods = ['POST'])
+def processes_page():
+    global num_proc
+    num_proc = int(request.form['num_proc'])
+    print('Number processes: %d' % num_proc)
+    return ''
+
+@app.route('/processes')
+def processes_dashboard():
+    global num_proc
+    return 'Client currently has %d processes running' % num_proc
 
 @app.route('/')
 def dashboard():

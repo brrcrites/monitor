6ec9f4a3f3db217baaf903ca2293662dc60fe345
brrcrites <brrcrites@gmail.com>
2019-07-25 22:23:37 -0700

    Harden config parsing, make config flag required

--- client.py
+++ client.py
@@ -7,19 +7,25 @@ import argparse
 from proc_info import ProcInfo
 from logs import LogInfo
 
+def parse_config_json(config_location):
+    try:
+        fd = open(config_location,'r')
+        file_str = fd.read()
+    except:
+        print('The config file located at "%s" failed to open or read' % config_location)
+        exit(-1) # TODO: raise an exception here instead
+    try:
+        file_json = json.loads(file_str)
+    except:
+        print('The config file located at "%s" failed to parse as valid JSON' % config_location)
+        exit(-1) # TODO: raise an exception here instead
+    return file_json
+
 parser = argparse.ArgumentParser(description = 'Client for sending monitoring data to central server')
-parser.add_argument('--config', type=str, help='Path to the JSON configuration file [required]')
+parser.add_argument('--config', type=str, help='Path to the JSON configuration file [required]', required=True)
 args = parser.parse_args()
 
-if not args.config:
-    print('Please provide a config file using the --config flag')
-    exit(-1) # TODO: raise an exception here instead
-
-# TODO: clean this up
-filehandle = open(args.config,'r')
-filestring = filehandle.read()
-print(filestring)
-config = json.loads(filestring)
+config = parse_config_json(args.config)
 
 info = ProcInfo(config)
 print(info.get_num_proc())

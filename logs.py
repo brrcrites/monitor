77986063adb6e9a742dba79ca0d6c2c9ff906940
brrcrites <brrcrites@gmail.com>
2019-07-25 22:24:21 -0700

    Allow client to report log retrieval failures

--- logs.py
+++ logs.py
@@ -10,10 +10,13 @@ class LogInfo:
         if 'send_logs' in self.config:
             logs_data = {}
             for log_path in self.config['send_logs']:
-                with open(log_path) as log_file:
-                    log_text = log_file.read()
-                    logs_data[log_path] = log_text
-            r = requests.post(url = 'http://127.0.0.1:5000/logs/send', data = logs_data )
+                try:
+                    with open(log_path) as log_file:
+                        log_text = log_file.read()
+                        logs_data[log_path] = log_text
+                except:
+                    logs_data[log_path] = '<font color="red">ERROR: The client failed to open the file "%s"</font>' % log_path
+                r = requests.post(url = 'http://127.0.0.1:5000/logs/send', data = logs_data )
             return logs_data
         return None
         
